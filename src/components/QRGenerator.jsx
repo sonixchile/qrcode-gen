0466c45f821a3d884779629e38c8046c3e747505
Ariel Hernandez <arielhernandezcl@gmail.com>
2024-12-11 13:04:57 -0300

    add disable on size input

--- src/components/QRGenerator.jsx
+++ src/components/QRGenerator.jsx
@@ -40,6 +40,7 @@ const QRGenerator = () => {
                 className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                 min="100"
                 max="500"
+                disabled
               />
             </div>
 
